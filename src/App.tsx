cb43a8870f7652f2978fb2737c9a0a40ee11c062
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2023-02-22 23:38:25 -0300

    build: add vite, react and pnpm

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+export function App() {
+    return (
+        <div>
+            0000 0000 0000 0000 Jane Appleseed 00/00 000 Cardholder Name e.g.
+            Jane Appleseed Card Number e.g. 1234 5678 9123 0000 Exp. Date
+            (MM/YY) MM YY CVC e.g. 123 Confirm
+            {/* <!-- Completed state start --> */}
+            Thank you! We&apos;ve added your card details Continue
+            <div className="attribution">
+                Challenge by
+                <a
+                    href="https://www.frontendmentor.io?ref=challenge"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Frontend Mentor
+                </a>
+                . Coded by <a href="#">Your Name Here</a>.
+            </div>
+        </div>
+    );
+}
