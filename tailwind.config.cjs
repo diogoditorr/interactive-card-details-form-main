1802be877ebb9bb43042ef8418f286bb0895ba48
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2023-02-24 11:12:34 -0300

    style: add project pallete colors and font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
     content: ["./src/**/*.{html,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "input-error": "hsl(0, 100%, 66%)",
+
+                "grayish-violet": {
+                    200: "hsl(270, 3%, 87%)",
+                    400: "hsl(279, 6%, 55%)",
+                    900: "hsl(278, 68%, 11%)",
+                },
+            },
+            fontFamily: {
+                sans: ["Space Grotesk"],
+            },
+            backgroundImage: {
+                "active-input-border":
+                    "linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%))",
+            },
+        },
+        screens: {
+            mobile: "375px",
+            desktop: "1440px",
+        },
     },
     plugins: [],
 };
